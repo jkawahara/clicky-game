58e7956ea52871ce800a853eee64e07b9aebf276
John Kawahara <kawaharajohn@gmail.com>
2019-01-23 23:53:53 -0800

    Updates

--- precache-manifest.6bc569e9ad8a22828870bc2a906fa724.js
+++ precache-manifest.6bc569e9ad8a22828870bc2a906fa724.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
   },
   {
-    "revision": "519f3fc28b043cbf38ec",
-    "url": "/clicky-game/static/js/main.519f3fc2.chunk.js"
+    "revision": "79e48c533f5b25ab3302",
+    "url": "/clicky-game/static/js/main.79e48c53.chunk.js"
   },
   {
     "revision": "fb9f72c6d9bfc22bcecb",
     "url": "/clicky-game/static/js/1.fb9f72c6.chunk.js"
   },
   {
-    "revision": "519f3fc28b043cbf38ec",
-    "url": "/clicky-game/static/css/main.b5591772.chunk.css"
+    "revision": "79e48c533f5b25ab3302",
+    "url": "/clicky-game/static/css/main.f52390e8.chunk.css"
   },
   {
-    "revision": "6b6f704477dd90c4eebfe55758fc8ea2",
+    "revision": "ce9d7be643645b1e5065336ecbf821cb",
     "url": "/clicky-game/index.html"
   }
 ];
